3c1ad383466898e2616a64f8b9d31861131cc263
AbdelfettahY <58116536+3ifrit@users.noreply.github.com>
2024-03-19 17:17:56 +0100

    Go

---
+++ Abdelfettah.Youbi.FeatureMatching.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.Json;
+
+using Abdelfettah.Youbi.FeatureMatching;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        if (args.Length < 2 || !File.Exists(args[0]) || !Directory.Exists(args[1]))
+        {
+            throw new ArgumentException("Usage: <path_to_object_image> <path_to_scene_images_directory>");
+        }
+
+        var imageData = await File.ReadAllBytesAsync(args[0]);
+        List<byte[]> scenesData = new List<byte[]>();
+
+        foreach(var sceneImagePath in Directory.EnumerateFiles(args[1]))
+        {
+            scenesData.Add(await File.ReadAllBytesAsync(sceneImagePath));
+        }
+
+
+        var detectObjectInScenesResults = await new ObjectDetection().DetectObjectInScenesAsync(imageData, scenesData);
+
+        foreach (var objectDetectionResult in detectObjectInScenesResults)
+        {
+            Console.WriteLine($"Points: {JsonSerializer.Serialize(objectDetectionResult.Points)}");
+        }
+    }
+}
---
+++ Abdelfettah.Youbi.FeatureMatching.WebApi/Program.cs
@@ -0,0 +1,47 @@
+using Abdelfettah.Youbi.FeatureMatching;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<ObjectDetection>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapPost("/FeatureMatching", async ([FromForm] IFormFileCollection files) =>
+{
+    if (files.Count != 2)
+        return Results.BadRequest();
+    using var objectSourceStream = files[0].OpenReadStream();
+    using var objectMemoryStream = new MemoryStream();
+    objectSourceStream.CopyTo(objectMemoryStream);
+    var imageObjectData = objectMemoryStream.ToArray();
+    using var sceneSourceStream = files[1].OpenReadStream();
+    using var sceneMemoryStream = new MemoryStream();
+    sceneSourceStream.CopyTo(sceneMemoryStream);
+    var imageSceneData = sceneMemoryStream.ToArray();
+
+    var singleImageList = new List<byte[]>();
+    singleImageList.Add(imageSceneData);
+    var result = await new ObjectDetection().DetectObjectInScenesAsync(imageObjectData, singleImageList);
+    
+    return Results.File(result[0].ImageData, "image/png");
+}).DisableAntiforgery();
+
+
+
+app.MapControllers();
+
+app.Run();
